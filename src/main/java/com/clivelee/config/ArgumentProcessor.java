d28a1514ef12b66c711329bb2c6ad81d02ea78cc
Clive HG Lee <hyungulee@gmail.com>
2018-06-05 00:48:12 -0400

    added regex for url validation + tests - apache common's url validator requires scheme, which we don't require

--- src/main/java/com/clivelee/config/ArgumentProcessor.java
+++ src/main/java/com/clivelee/config/ArgumentProcessor.java
@@ -1,10 +1,16 @@
 package com.clivelee.config;
 
-import org.apache.http.util.TextUtils;
+import com.google.common.annotations.VisibleForTesting;
 
-import java.net.URI;
+import javax.annotation.Nonnull;
+import java.util.regex.Pattern;
+
 
 public final class ArgumentProcessor {
+
+    private static String urlRegex = "^(http(s)?://)?(([a-z0-9]+)[.])+[a-z0-9]+$";
+    private static Pattern p = Pattern.compile(urlRegex, Pattern.CASE_INSENSITIVE);
+
     public Configuration parseArguments(String[] args) {
 
         if (args == null || args.length != 1) {
@@ -13,12 +19,14 @@ public final class ArgumentProcessor {
 
         String urlString = args[0];
 
-        URI uri = URI.create(urlString);
-
-        if (TextUtils.isEmpty(uri.getScheme())) {
-            urlString = "http://" + urlString;
+        if (!isValidDomain(urlString)) {
+            throw new UrlMalformed(urlString);
         }
-
         return new Configuration(urlString);
+    }
+
+    @VisibleForTesting
+    static boolean isValidDomain(@Nonnull String urlString) {
+        return p.matcher(urlString).find();
     }
 }
--- src/test/java/com/clivelee/config/ArgumentProcessorTest.java
+++ src/test/java/com/clivelee/config/ArgumentProcessorTest.java
@@ -1,11 +1,16 @@
 package com.clivelee.config;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 import static com.google.common.truth.Truth.assertThat;
 
 
 public class ArgumentProcessorTest {
+
+    @Rule
+    public final ExpectedException exception = ExpectedException.none();
 
     @Test
     public void parseArguments_valid() {
@@ -17,11 +22,69 @@ public class ArgumentProcessorTest {
     }
 
     @Test
-    public void parseArguments_no_scheme() {
+    public void parseArguments_noScheme() {
         String[] args = {"www.example.com"};
 
         Configuration config = new ArgumentProcessor().parseArguments(args);
 
-        assertThat(config.domainName()).isEqualTo("http://www.example.com");
+        assertThat(config.domainName()).isEqualTo("www.example.com");
+    }
+
+    @Test
+    public void parseArguments_emptyArgs() {
+        String[] args = {};
+
+        exception.expect(UrlNotSpecifiedException.class);
+        new ArgumentProcessor().parseArguments(args);
+    }
+
+    @Test
+    public void parseArguments_longArgs() {
+        String[] args = {"www.example.com", "oops"};
+        exception.expect(UrlNotSpecifiedException.class);
+        new ArgumentProcessor().parseArguments(args);
+    }
+
+    @Test
+    public void parseArguments_malformedUrl() {
+        String[] args = {"htttp://www.example.com"};
+        exception.expect(UrlMalformed.class);
+        new ArgumentProcessor().parseArguments(args);
+    }
+
+    @Test
+    public void isValidDomain_noScheme() {
+        String testDomain = "www.example.com";
+        assertThat(ArgumentProcessor.isValidDomain(testDomain)).isTrue();
+    }
+
+    @Test
+    public void isValidDomain_withHttpScheme() {
+        String testDomain = "http://www.example.com";
+        assertThat(ArgumentProcessor.isValidDomain(testDomain)).isTrue();
+    }
+
+    @Test
+    public void isValidDomain_withHttpsScheme() {
+        String testDomain = "https://gobbledygook.co";
+        assertThat(ArgumentProcessor.isValidDomain(testDomain)).isTrue();
+    }
+
+    @Test
+    public void isValidDomain_invalidSpecial() {
+        String testDomain = "gobbledygook.!!!.com";
+        assertThat(ArgumentProcessor.isValidDomain(testDomain)).isFalse();
+    }
+
+    @Test
+    public void isValidDomain_invalid() {
+        String testDomain = "gobbledygook";
+        assertThat(ArgumentProcessor.isValidDomain(testDomain)).isFalse();
+    }
+
+    @Test
+    public void isValidDomain_invalidTwoPeriods() {
+        String testDomain = "gobbledygook..com";
+        assertThat(ArgumentProcessor.isValidDomain(testDomain)).isFalse();
     }
 }
